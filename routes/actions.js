def70ee95d73081dc18bf27bf6437b400d6c6de6
Noah <noah.stoeckel@gmail.com>
2019-03-09 12:16:29 -0500

    adds insert actions endpoint

--- routes/actions.js
+++ routes/actions.js
@@ -13,4 +13,21 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  const { description, notes, completed, project_id } = req.body;
+  try {
+    if (!description || !notes || completed === undefined || !project_id) {
+      res.status(400).json({
+        error:
+          "Please provide a description, notes, and completed status for action."
+      });
+    } else {
+      const action = await db.insert(req.body);
+      res.status(201).json(action);
+    }
+  } catch (e) {
+    res.status(500).json({ error: "Could Not Create the User" });
+  }
+});
+
 module.exports = router;
