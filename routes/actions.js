ca0d1f83d57780c0917ab289efcf37f3dfc09af1
Noah <noah.stoeckel@gmail.com>
2019-03-09 12:12:23 -0500

    adds actions endpoint

--- routes/actions.js
+++ routes/actions.js
@@ -2,4 +2,15 @@ const express = require("express");
 const router = express.Router();
 const db = require("../data/helpers/actionModel");
 
+router.get("/", async (req, res) => {
+  try {
+    const actions = await db.get();
+    res.status(200).json(actions);
+  } catch (e) {
+    res
+      .status(500)
+      .json({ error: "The actions information could not be retrieved." });
+  }
+});
+
 module.exports = router;
