e1ba412f3149f5d3dba2d0484a0429fe20978104
Noah <noah.stoeckel@gmail.com>
2019-03-09 12:30:26 -0500

    adds get all projects endpoint

--- routes/projects.js
+++ routes/projects.js
@@ -2,4 +2,15 @@ const express = require("express");
 const router = express.Router();
 const db = require("../data/helpers/projectModel");
 
+router.get("/", async (req, res) => {
+  try {
+    const projects = await db.get();
+    res.status(200).json(projects);
+  } catch (e) {
+    res
+      .status(500)
+      .json({ error: "The projects information could not be retrieved." });
+  }
+});
+
 module.exports = router;
